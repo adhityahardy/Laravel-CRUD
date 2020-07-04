ca36458dfe98e7dde598eef04d13ecae1453c289
beginner75 <44922464+adhityahardy@users.noreply.github.com>
2020-07-04 14:56:36 +0700

    first commit

---
+++ resources/views/content/isijawaban.php
@@ -0,0 +1,10 @@
+@extends('adminlte.master')
+
+@section('content')
+
+<div class="container-md mt-3 border">
+        <label for="judul">Jawaban:</label>
+        {{$items->isi}}
+</div>  
+    
+@endsection
